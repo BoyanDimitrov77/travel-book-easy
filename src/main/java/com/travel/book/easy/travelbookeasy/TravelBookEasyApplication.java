df6d577c684b266bc078ccb57a3b60e645e934a1
Boyan Dimitrov <boyan.dimitrov@scalefocus.com>
2018-11-03 15:55:27 +0200

    Configuration

---
+++ src/main/java/com/travel/book/easy/travelbookeasy/TravelBookEasyApplication.java
@@ -0,0 +1,12 @@
+package com.travel.book.easy.travelbookeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookEasyApplication.class, args);
+	}
+}
